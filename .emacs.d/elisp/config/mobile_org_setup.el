f718a61b4f28a6baac11faacc625569e3b8cc62d
Steen Manniche <steen@manniche.net>
2012-02-14 20:55:32 +0100

    separating mobileorg configuration into a separate file

---
+++ .emacs.d/elisp/config/mobile_org_setup.el
@@ -0,0 +1,7 @@
+(require 'org-install)
+
+(setq org-directory "~/.org/mobile")
+(setq org-mobile-directory "~/Dropbox/MobileOrg/")
+(setq org-mobile-inbox-for-pull "~/.org/mobile/inbox.org" )
+
+(provide 'mobile_org_setup)
