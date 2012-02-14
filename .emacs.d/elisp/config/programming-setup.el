b09da0c5d0adaa03bc55266b0a6c7dc35c88d805
Steen Manniche <steen@manniche.net>
2012-02-14 20:57:48 +0100

    moving programming related setup to a separate file

---
+++ .emacs.d/elisp/config/programming-setup.el
@@ -0,0 +1,25 @@
+;; yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/elpa/yasnippet")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/elpa/yasnippet/snippets")
+(yas/global-mode 1)
+
+
+;;; clojure mode setup
+(add-to-list 'auto-mode-alist
+ (cons (concat "\\." (regexp-opt '("clj" "cljs") t) "\\'")
+ 'clojure-mode))
+(add-hook 'clojure-mode-hook (load-file "~/.emacs.d/elisp/config/emacs-clojure.el"))
+;;; end clojure mode setup
+
+;;; java mode setup
+(add-to-list 'auto-mode-alist
+ (cons (concat "\\." (regexp-opt '("java" "jsp") t) "\\'")
+ 'java-mode))
+(add-hook 'java-mode-hook (load-file "~/.emacs.d/elisp/config/emacs-java.el"))
+;;; end java mode setup
+
+
+(provide 'programming-setup)
