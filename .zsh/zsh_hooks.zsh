087a0b60fe9749a16afe710e823916da69d63c49
Steen Manniche <steen@manniche.net>
2013-02-06 20:27:19 +0100

    removing functions that were messing up the stdout output for piping

--- .zsh/zsh_hooks.zsh
+++ .zsh/zsh_hooks.zsh
@@ -7,14 +7,14 @@ function precmd {
   echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
 }
 
-function set_running_app {
-  printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
-}
+# function set_running_app {
+#   printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
+# }
 
-function preexec {
-  set_running_app
-}
+# function preexec {
+#   set_running_app
+# }
 
-function postexec {
-  set_running_app
-}
+# function postexec {
+#   set_running_app
+# }
