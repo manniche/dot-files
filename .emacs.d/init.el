4aacb80de4831b69eb2ddabf9a4884f1623ea1c3
Steen Manniche <steen@manniche.net>
2015-10-28 20:03:50 +0100

    adding markdown mode to list of packages to install with emacs if not present

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
 ;; elpa setup
 ;;;;;;;;;;;;;
 ;(require 'package)
-(setq package-list '(yasnippet clojure-mode evil projectile highlight highlight-parentheses powerline evil-leader))
+(setq package-list '(yasnippet clojure-mode evil projectile highlight highlight-parentheses powerline evil-leader markdown-mode))
 
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
 			 ("elpy" . "http://jorgenschaefer.github.io/packages/")
