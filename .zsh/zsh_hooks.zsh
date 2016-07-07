d94e8090b54700d2667a87cce7005ef2ae46a336
Steen Manniche <steen@manniche.net>
2016-07-07 13:55:13 +0200

    putting a rudimentary timer in the RPROMPT

--- .zsh/zsh_hooks.zsh
+++ .zsh/zsh_hooks.zsh
@@ -1,3 +1,7 @@
+function preexec() {
+  timer=${timer:-$SECONDS}
+}
+
 function precmd {
   # vcs_info
   # Put the string "hostname::/full/directory/path" in the title bar:
@@ -5,6 +9,12 @@ function precmd {
 
   # Put the parentdir/currentdir in the tab
   echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
+
+  if [ $timer ]; then
+    timer_show=$(($SECONDS - $timer))
+    export RPROMPT="%F{cyan}${timer_show}s %{$reset_color%}"
+    unset timer
+  fi
 }
 
 # function set_running_app {
