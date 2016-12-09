ff20c949ea3623e55d4e3175d2b9d8400cda5c38
Steen Manniche <steen@manniche.net>
2016-12-09 16:06:19 +0100

    listing default emacs packages to install on a clean emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,3 +45,17 @@
 (require 'evil-keybindings)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elpy yasnippet yaml-mode projectile powerline markdown-mode magit linum-relative js2-mode highlight-parentheses highlight folding evil-nerd-commenter evil-leader evil-commentary dockerfile-mode csv-mode clojure-mode auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
