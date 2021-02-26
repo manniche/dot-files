4b8864b7b2cfdd472c784922ab8a58a76a2d07cf
Steen Manniche <steen@manniche.net>
2021-02-26 15:17:44 +0100

    add typescript mode to std modes

--- configs/doom.d/custom.el
+++ configs/doom.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(nginx-mode pyenv-mode virtualenv auto-virtualenv)))
+ '(package-selected-packages
+   '(typescript-mode nginx-mode pyenv-mode virtualenv auto-virtualenv)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
