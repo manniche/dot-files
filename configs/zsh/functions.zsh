e489b11c5b93664245d662b5275e823e828f698c
Steen Manniche <steen@manniche.net>
2021-03-20 21:46:25 +0100

    install new jump command

--- configs/zsh/functions.zsh
+++ configs/zsh/functions.zsh
@@ -193,17 +193,3 @@ function lb() {
     vim ~/logbook/$(date '+%Y-%m-%d').md
 }
 
-export MARKPATH=$HOME/.marks
-function jump {
-    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
-}
-function mark {
-    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
-}
-function unmark {
-    rm -i "$MARKPATH/$1"
-}
-function marks {
-    ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
-}
-
